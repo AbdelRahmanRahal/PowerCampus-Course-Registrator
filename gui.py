5fd579dc5328313646030b7db9ad69a8faca1417
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2023-09-14 21:31:56 +0300

    Working on the script. Better GUI. Cleaner code.

--- gui.py
+++ gui.py
@@ -1,7 +1,67 @@
-from PySide6.QtCore import Qt
+import sys, time
+from datetime import datetime
+
+from PySide6.QtCore import Qt, QThread, Signal
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QHBoxLayout, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QLineEdit, QTextEdit, QFileDialog
-import sys
+from PySide6.QtWidgets import (
+	QApplication,
+	QFileDialog,
+	QHBoxLayout, 
+	QLabel,
+	QLineEdit,
+	QMainWindow,
+	QPushButton, 
+	QTextEdit,
+	QVBoxLayout,
+	QWidget
+)
+from selenium.common.exceptions import NoSuchDriverException
+
+from automation import PowerCampusAutomation
+
+
+def CURRENT_TIME():
+	return datetime.now().strftime("<small>[%I:%M %p]</small>")
+
+
+class AutomationThread(QThread):
+	log_signal = Signal(str)
+
+
+	def __init__(self, driver_path, username, password):
+		super().__init__()
+		self.driver_path = driver_path
+		self.username = username
+		self.password = password
+
+
+	def run(self) -> None:
+		'''
+		This method gets called when the thread starts (i.e. `___.start()`)
+		'''
+		try:
+			self.log_signal.emit(
+				f"{CURRENT_TIME()} ⚙️ Initialising WebDriver..."
+			)
+			start_time = time.time()
+			automation = PowerCampusAutomation(self.driver_path)
+			end_time = time.time()
+			elapsed_time = end_time - start_time
+			self.log_signal.emit(
+				f"{CURRENT_TIME()} ✅ WebDriver initialised successfully. (Took {elapsed_time:.2f}s)"
+			)
+
+			automation.login(self.username, self.password)
+			automation.register()
+			automation.quit()
+		except NoSuchDriverException:
+			self.log_signal.emit(
+				f"{CURRENT_TIME()} ❌ WebDriver corrupted, incompatible, unobtainable, or doesn't exist."
+			)
+		except OSError:
+			self.log_signal.emit(
+				f"{CURRENT_TIME()} ❌ WebDriver selected is invalid."
+			)
 
 
 class MainWindow(QMainWindow):
@@ -10,20 +70,22 @@ class MainWindow(QMainWindow):
 
 		self.setWindowTitle("PowerCampus Course Registrator")
 		self.resize(720, 480)
-		self.setWindowIcon(QIcon('icon.png'))
+		self.setWindowIcon(QIcon("icon.png"))
 
 		layout: QVBoxLayout = QVBoxLayout()
 
 		# -------------------------- Driver path lookup area ------------------------- #
-		self.driver_label: QLabel = QLabel('Select path for webdriver (preferably ChromeDriver):')
+		self.driver_label: QLabel = QLabel(
+			"<h4>Select path for WebDriver (preferably ChromeDriver):</h4>"
+		)
 		self.driver_label.setFixedHeight(15)
 
 		self.driver_path_textbox: QLineEdit = QLineEdit()
-		self.driver_path_textbox.setText('chromedriver/chromedriver.exe')
+		self.driver_path_textbox.setText("chromedriver/chromedriver.exe")
 		self.driver_path_textbox.setPlaceholderText("ChromeDriver Path")
 		
 		self.driver_path_lookup: QPushButton = QPushButton("Find Driver")
-		self.driver_path_lookup.clicked.connect(self.select_file)
+		self.driver_path_lookup.clicked.connect(self.lookup_driver)
 
 
 		driver_path_layout: QHBoxLayout = QHBoxLayout()
@@ -36,10 +98,12 @@ class MainWindow(QMainWindow):
 		layout.addLayout(driver_path_layout)
 
 		# ---------------------- Username and password textboxes --------------------- #
-		self.login_label: QLabel = QLabel('Fill out your Nile University PowerCampus login information:')
+		self.login_label: QLabel = QLabel(
+			"<h4>Fill out your Nile University PowerCampus login information:</h4>"
+		)
 		self.login_label.setFixedHeight(15)
 
-		self.username_label: QLabel = QLabel('Username:     ')
+		self.username_label: QLabel = QLabel("Username:     ")
 		self.username_textbox: QLineEdit = QLineEdit()
 		self.username_textbox.setPlaceholderText("Username")
 
@@ -50,7 +114,7 @@ class MainWindow(QMainWindow):
 		username_layout.addStretch(1)
 
 
-		self.password_label: QLabel = QLabel('Password:      ')
+		self.password_label: QLabel = QLabel("Password:      ")
 		self.password_textbox: QLineEdit = QLineEdit()
 		self.password_textbox.setPlaceholderText("Password")
 		self.password_textbox.setEchoMode(QLineEdit.EchoMode.Password)
@@ -78,18 +142,36 @@ class MainWindow(QMainWindow):
 		self.log_area.setFixedHeight(150)
 		layout.addWidget(self.log_area)
 
+		# ---------------------- Containerising the main layout ---------------------- #
 		container: QWidget = QWidget()
 		container.setLayout(layout)
 		container.setContentsMargins(15, 15, 15, 15)
 
 		self.setCentralWidget(container)
+	
 
-	def select_file(self):
-		file_path, _ = QFileDialog.getOpenFileName(self, "Select File")
-		self.log_area.append(f"File selected: {file_path}")
+	def lookup_driver(self):
+		file_path, _ = QFileDialog.getOpenFileName(self, "Select WebDriver")
+		if file_path:
+			self.driver_path_textbox.clear()
+			self.driver_path_textbox.setText(file_path)
+		else:
+			
+			self.log_area.append(
+				f"{CURRENT_TIME()} ℹ️ No file was selected."
+			)
+
 
 	def run(self):
-		self.log_area.append(f"Username: {self.username_textbox.text()}, Password: {self.password_textbox.text()}")
+		driver_path = self.driver_path_textbox.text()
+		username = self.username_textbox.text()
+		password = self.password_textbox.text()
+
+		self.automation_thread = AutomationThread(driver_path, username, password)
+		self.automation_thread.log_signal.connect(self.log_area.append)
+		self.automation_thread.start()
+	
+
 
 app = QApplication(sys.argv)
 
